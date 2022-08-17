fc9ea60351a31fafe0d33fd698748e7dbaae2509
Tom Dierenfeld <tom@hobbitshop24.biz>
2022-08-17 20:21:39 +0200

    adds csv file reader
    read csv file and convert content to dict, while also sanitizing
    fields (casting values to float, trimming whitespace)

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
+import csv
 import re
 import sys
 from ast import literal_eval
 
+DATA_CSV = "data.csv"
+
 
 def main(param):
     try:
-        param = validate(param)
+        param = _validate(param)
     except ValueError as ve:
         print(f"Cannot parse input: {ve}", file=sys.stderr)
         sys.exit(1)
 
 
-def validate(param):
+def _validate(param):
     if type(param) == str:
         param = literal_eval(param)  # throws a ValueError if un-eval-able
     if type(param) != dict or "data" not in param.keys():
@@ -34,6 +37,31 @@ def validate(param):
             # I assume that the query can either be 'power' or 'value'
             raise ValueError(f"Malformed query: {query}")
     return param
+
+
+def _get_row_by_date(date):
+    data = _read_database()
+
+
+def _read_database(csvfile=DATA_CSV):
+    # convert csv to dict
+    data = []
+    with open(csvfile) as f:
+        for item in csv.DictReader(f):
+            data.append(dict(item))
+    if not data:
+        raise ValueError(f"File {DATA_CSV} appears to be empty")
+    # trim whitespace
+    tmpdata = data
+    data = []
+    for d in tmpdata:
+        data.append({k.strip(): v for (k, v) in d.items()})
+    # convert values to int
+    for d in data:
+        for key in d:
+            if key.startswith("value"):
+                d[key] = float(d[key])
+    return data
 
 
 if __name__ == '__main__':
--- test_main.py
+++ test_main.py
@@ -1,6 +1,8 @@
+from unittest import mock
+
 import pytest
 
-from main import validate
+from main import _validate, _read_database
 
 params1 = {
     "data": {
@@ -49,18 +51,32 @@ params3 = {
     }
 }
 
+datacsv = """date_from, value_up_to_10_kwp, value_up_to_30_kwp, value_up_to_40_kwp
+2019-01-01, 0.5062, 0.4962, 0.4862
+2019-02-01, 0.4810, 0.4710, 0.4610
+"""
+
 
 class TestValidate:
     def test_validate_ok(self):
-        result = validate(params1)
+        result = _validate(params1)
         assert result["data"]["attributes"]["list"][0]["power"] == "12"
 
     def test_validate_str_conv_ok(self):
-        result = validate(params2)
+        result = _validate(params2)
         assert type(result) == dict
 
     def test_validate_invalid_value(self):
         with pytest.raises(ValueError) as ve:
-            validate(params3)
+            _validate(params3)
 
         assert "Malformed query" in str(ve)
+
+
+class TestDatabaseReader:
+    def test_read_database_ok(self):
+        mocked_open_function = mock.mock_open(read_data=datacsv)
+        with mock.patch("builtins.open", mocked_open_function):
+            data = _read_database()
+            assert len(data) == 2
+            assert data[0]["value_up_to_10_kwp"] < 1.0
